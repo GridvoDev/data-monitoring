9531403d868e263584aed3f3da9a324c965979bb
Johnlinmadan <772181827@qq.com>
2017-01-19 21:56:42 +0800

    添加实时数据推送规则监控

--- test/application/service/monitorServiceTest.js
+++ test/application/service/monitorServiceTest.js
@@ -3,6 +3,7 @@ const _ = require('underscore');
 const should = require('should');
 const muk = require('muk');
 const MonitorService = require('../../../lib/application/service/monitorService');
+const {createMonitorService} = require('../../../lib/domain');
 
 describe('MonitorService use case test', ()=> {
     let service;
@@ -23,14 +24,12 @@ describe('MonitorService use case test', ()=> {
             });
             it('if monitor already existed,is fail', done=> {
                 let mockMonitorRepository = {};
-                mockMonitorRepository.getMonitorByID = (monitorID, {}, callback)=> {
+                mockMonitorRepository.getMonitorByID = (monitorID, traceContext, callback)=> {
                     callback(null, {});
                 };
                 muk(service, "_monitorRepository", mockMonitorRepository);
                 let dataSourceData = {
-                    id: "/station/datatype/other",
-                    station: "stationID",
-                    lessee: "lesseeID"
+                    id: "station-rain-other"
                 };
                 service.registerMonitor(dataSourceData, {}, (err, isSuccess)=> {
                     if (err) {
@@ -50,9 +49,7 @@ describe('MonitorService use case test', ()=> {
                 };
                 muk(service, "_monitorRepository", mockMonitorRepository);
                 let dataSourceData = {
-                    id: "/station/datatype/other",
-                    station: "stationID",
-                    lessee: "lesseeID"
+                    id: "station-rain-other"
                 };
                 service.registerMonitor(dataSourceData, {}, (err, isSuccess)=> {
                     if (err) {
@@ -60,6 +57,97 @@ describe('MonitorService use case test', ()=> {
                     }
                     isSuccess.should.be.eql(true);
                     done();
+                });
+            });
+        });
+    });
+    describe('#monitoringData(originalData, traceContext, callback)', ()=> {
+        context('monitoring original data', ()=> {
+            it('if original data no "s t v",is fail', done=> {
+                let originalData = {};
+                service.monitoringData(originalData, {}, (err, isSuccess)=> {
+                    if (err) {
+                        done(err);
+                    }
+                    isSuccess.should.be.eql(false);
+                    done();
+                });
+            });
+            it('if no this monitor,is fail', done=> {
+                let mockMonitorRepository = {};
+                mockMonitorRepository.getMonitorByID = (monitorID, traceContext, callback)=> {
+                    callback(null, null);
+                };
+                muk(service, "_monitorRepository", mockMonitorRepository);
+                let originalData = {};
+                originalData.s = "no-data-source";
+                originalData.t = (new Date()).getTime();
+                originalData.v = 1;
+                service.monitoringData(originalData, {}, (err, isSuccess)=> {
+                    if (err) {
+                        done(err);
+                    }
+                    isSuccess.should.be.eql(false);
+                    done();
+                });
+            });
+            it('is success', done=> {
+                let currentDoneCount = 0;
+
+                function doneMore(err) {
+                    currentDoneCount++;
+                    if (currentDoneCount == 2) {
+                        if (err) {
+                            done(err);
+                        }
+                        else {
+                            done();
+                        }
+                    }
+                };
+                let mockMonitorRepository = {};
+                mockMonitorRepository.getMonitorByID = (monitorID, traceContext, callback)=> {
+                    let monitor = createMonitorService().loadMonitor({
+                        id: "station-rain-other",
+                        dataType: 'rain',
+                        rules: {
+                            realTimePublish: {
+                                id: 'realTimePublish',
+                                name: '实时推送',
+                                publishSpace: 60000,
+                                isOpen: true
+                            }
+                        },
+                        monitoringItems: {
+                            lastPublishTimestamp: 1
+                        }
+                    });
+                    callback(null, monitor);
+                };
+                mockMonitorRepository.save = (monitor, traceContext, callback)=> {
+                    callback(null, true);
+                };
+                muk(service, "_monitorRepository", mockMonitorRepository);
+                let timestamp = (new Date()).getTime();
+                let mockMessageProducer = {};
+                mockMessageProducer.produceDataPublishTopicMessage = (message, traceContext, callback)=> {
+                    message.dataSource.should.equal("station-rain-other");
+                    message.timestamp.should.equal(timestamp);
+                    message.value.should.equal(1);
+                    doneMore();
+                }
+                muk(service, "_messageProducer", mockMessageProducer);
+                let originalData = {
+                    s: "station-rain-other",
+                    t: timestamp,
+                    v: 1
+                };
+                service.monitoringData(originalData, {}, (err, isSuccess)=> {
+                    if (err) {
+                        doneMore(err);
+                    }
+                    isSuccess.should.be.eql(true);
+                    doneMore();
                 });
             });
         });
